3a4e91839f60053915d3fce491f7ba37a13804a9
D2ceCube <assatulladias@gmail.com>
2023-10-21 18:04:19 +0200

    Added main page and viewPage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.journalapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.journalapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -44,4 +44,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.google.android.material:material:1.1.0")
 }
--- app/src/main/java/com/example/journalapp/MainActivity.kt
+++ app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.journalapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.example.journalapp.fragments.AnalysisFragment
+import com.example.journalapp.fragments.HomeFragment
+import com.example.journalapp.fragments.TransactionsFragment
+import com.example.journalapp.fragments.adapters.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private val viewPager:ViewPager
+        get() = findViewById(R.id.viewPager)
+    private val tabs:TabLayout
+        get() = findViewById(R.id.tabs)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpTabs()
     }
+
+
+    private fun setUpTabs() {
+
+
+
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+        adapter.addFragment(HomeFragment(),"Home")
+        adapter.addFragment(TransactionsFragment(), "Transactions")
+        adapter.addFragment(AnalysisFragment(),"Analysis")
+
+        viewPager.adapter = adapter
+        tabs.setupWithViewPager(viewPager)
+
+        // Assign icons to fragments
+        tabs.getTabAt(0)!!.setIcon(R.drawable.baseline_home_24)
+        tabs.getTabAt(1)!!.setIcon(R.drawable.baseline_currency_exchange_24)
+        tabs.getTabAt(2)!!.setIcon(R.drawable.baseline_bar_chart_24)
+
+    }
+
 }
