e17863cd0d24105eda6afee2ea7f5ab352947181
Kambulat <kambulat11@gmail.com>
2023-10-25 17:01:14 +0200

    modifed 'add transaction' page and was bound 'back' button

--- app/src/main/java/com/example/journalapp/fragments/AddTransactionFragment.kt
+++ app/src/main/java/com/example/journalapp/fragments/AddTransactionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.example.journalapp.R
 
@@ -17,6 +18,18 @@ class AddTransactionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_transaction, container, false)
+        val view =  inflater.inflate(R.layout.fragment_add_transaction, container, false)
+
+        val arrowBackBtn = view.findViewById<ImageView>(R.id.arrowBackBtn)
+
+        arrowBackBtn.setOnClickListener {
+            // Pop the fragment from the back stack to return to the previous fragment
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+
+        return view
     }
+
+
 }
